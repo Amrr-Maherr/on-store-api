5788fd6bcd3e3a36ab0afea3544fb31b2f9ff086
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-08-28 22:56:42 +0300

    Update

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express, { type Express, type Request, type Response } from 'express';
-
+require('dotenv').config()
 const app: Express = express();
-
+const port = process.env.APP_PORT
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log(`running on port${3000}`);
+app.listen(port, () => {
+  console.log(`running on port ${port}`);
 });
