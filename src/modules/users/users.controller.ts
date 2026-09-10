2785b310e8ca610d8022b9fb1d8a5a74a6e55837
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-09-10 20:39:11 +0300

    Add users module scaffold

---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,9 @@
+﻿import { type Request, type Response } from 'express';
+export const createUser = () => {
+}
+export const deleteUser = () => {
+}
+export const updateUser = () => {
+}
+export const getUsers = () => {
+}
---
+++ src/modules/users/users.routes.ts
@@ -0,0 +1,18 @@
+﻿import { Router } from "express";
+
+import {
+    createUser,
+    getUsers,
+    deleteUser,
+    updateUser,
+} from "./users.controller.js";
+
+export const usersRouter: Router = Router();
+
+usersRouter.get("/", getUsers);
+
+usersRouter.post("/", createUser);
+
+usersRouter.delete("/:id", deleteUser);
+
+usersRouter.patch("/:id", updateUser);
