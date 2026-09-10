929dc0e3621b36bf0e63db873c893b4d3488c088
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-09-10 20:39:14 +0300

    Add wishlist module scaffold

---
+++ src/modules/wishlist/wishlist.routes.ts
@@ -0,0 +1,18 @@
+﻿import { Router } from "express";
+
+import {
+    createWishlist,
+    getWishlists,
+    deleteWishlist,
+    updateWishlist,
+} from "./wishlist.controller.js";
+
+export const wishlistRouter: Router = Router();
+
+wishlistRouter.get("/", getWishlists);
+
+wishlistRouter.post("/", createWishlist);
+
+wishlistRouter.delete("/:id", deleteWishlist);
+
+wishlistRouter.patch("/:id", updateWishlist);
