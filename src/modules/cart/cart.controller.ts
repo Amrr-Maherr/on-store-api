3a1755103191a9dd6218271d3bccd9944ea4d418
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-09-10 20:38:45 +0300

    Add cart module scaffold

---
+++ src/modules/cart/cart.controller.ts
@@ -0,0 +1,9 @@
+﻿import { type Request, type Response } from 'express';
+export const createCart = () => {
+}
+export const deleteCart = () => {
+}
+export const updateCart = () => {
+}
+export const getCarts = () => {
+}
---
+++ src/modules/cart/cart.routes.ts
@@ -0,0 +1,18 @@
+﻿import { Router } from "express";
+
+import {
+    createCart,
+    getCarts,
+    deleteCart,
+    updateCart,
+} from "./cart.controller.js";
+
+export const cartRouter: Router = Router();
+
+cartRouter.get("/", getCarts);
+
+cartRouter.post("/", createCart);
+
+cartRouter.delete("/:id", deleteCart);
+
+cartRouter.patch("/:id", updateCart);
