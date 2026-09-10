0cace690bcdea2fe247269184996dd41edad628d
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-09-10 19:41:30 +0300

    feat: add categories router to the Express app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { type Express, type Request, type Response } from 'express';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
+import { categoriesRouter } from './modules/categories/categories.routes.js';
 dotenv.config();
 const app: Express = express();
 const port = process.env.APP_PORT
+app.use("/api/v1/categories", categoriesRouter)
 connectDB().then(() => {
   app.listen(port, () => {
     console.log(`running on port ${port}`);
