cc6899bd49c2babb0c7c9d432e270534a4d2b9c3
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-09-10 20:38:41 +0300

    Add brands module scaffold

---
+++ src/modules/brands/brands.controller.ts
@@ -0,0 +1,9 @@
+﻿import { type Request, type Response } from 'express';
+export const createBrand = () => {
+}
+export const deleteBrand = () => {
+}
+export const updateBrand = () => {
+}
+export const getBrands = () => {
+}
