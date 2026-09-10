5aabe6b44a12bf1981fd281432ae67a1af0dcaee
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-09-10 20:38:54 +0300

    Add orders module scaffold

---
+++ src/modules/orders/orders.routes.ts
@@ -0,0 +1,18 @@
+﻿import { Router } from "express";
+
+import {
+    createOrder,
+    getOrders,
+    deleteOrder,
+    updateOrder,
+} from "./orders.controller.js";
+
+export const ordersRouter: Router = Router();
+
+ordersRouter.get("/", getOrders);
+
+ordersRouter.post("/", createOrder);
+
+ordersRouter.delete("/:id", deleteOrder);
+
+ordersRouter.patch("/:id", updateOrder);
