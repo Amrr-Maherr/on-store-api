ab2540bb1b2092d81784f2ba0c14b50aa4f93a0d
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-09-11 16:51:16 +0300

    feat: add request validation middleware to brand routes

--- src/modules/brands/brands.routes.ts
+++ src/modules/brands/brands.routes.ts
@@ -1,13 +1,19 @@
 ﻿import { Router } from 'express';
 
 import { createBrand, getBrands, deleteBrand, updateBrand } from './brands.controller.js';
+import {
+  updateBrandValidation,
+  createBrandValidation,
+  validateBrandId,
+} from './brands.validation.js';
+import { validateRequest } from '../../middlewares/validateRequest.js';
 
 export const brandsRouter: Router = Router();
 
 brandsRouter.get('/', getBrands);
 
-brandsRouter.post('/', createBrand);
+brandsRouter.post('/', createBrandValidation, validateRequest, createBrand);
 
-brandsRouter.delete('/:id', deleteBrand);
+brandsRouter.delete('/:id', validateBrandId, validateRequest, deleteBrand);
 
-brandsRouter.patch('/:id', updateBrand);
+brandsRouter.patch('/:id', validateBrandId, updateBrandValidation, validateRequest, updateBrand);
