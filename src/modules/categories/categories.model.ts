33a674f666546f7a168e5da3e0eaafabbed28bc6
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-09-11 16:03:50 +0300

    feat: define Category model with schema and export

--- src/modules/categories/categories.model.ts
+++ src/modules/categories/categories.model.ts
@@ -1,1 +1,19 @@
-﻿
+﻿import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const CategorySchema = new Schema(
+    {
+        name: { type: String, required: true, trim: true },
+        image: { type: String, required: true },
+        slug: { type: String, required: true, unique: true, trim: true },
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+);
+
+const Category = mongoose.model('Category', CategorySchema);
+
+export default Category;
