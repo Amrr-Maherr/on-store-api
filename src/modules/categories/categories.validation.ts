0bc9d9ffdf761b15cdbeed05b58df3e93aa4a6d1
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-09-11 16:03:43 +0300

    feat: add validation for category creation, update, and ID validation

--- src/modules/categories/categories.validation.ts
+++ src/modules/categories/categories.validation.ts
@@ -1,1 +1,18 @@
-﻿
+﻿import { body, param } from 'express-validator';
+
+export const createCategoryValidation = [
+    body('name').notEmpty().withMessage('Category name is required'),
+    body('slug').notEmpty().withMessage('Category slug is required'),
+    body('image').notEmpty().withMessage('Category image is required'),
+];
+export const updateCategoryValidation = [
+    body('name').notEmpty().optional().withMessage('Category name is required'),
+    body('image').notEmpty().optional().withMessage('Category image is required'),
+];
+export const validateCategoryId = [
+    param("id")
+        .notEmpty()
+        .withMessage("Category ID is required")
+        .isMongoId()
+        .withMessage("Invalid category ID"),
+];
