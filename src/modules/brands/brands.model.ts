5a57cc7b71ec932301c0ea94ce93e6a440f4a03c
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-09-11 16:50:56 +0300

    feat: define Brand model with schema and export

--- src/modules/brands/brands.model.ts
+++ src/modules/brands/brands.model.ts
@@ -1,1 +1,24 @@
-﻿
+﻿import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const BrandSchema = new Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    image: { type: String, required: true },
+    slug: { type: String, required: true, unique: true, trim: true },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const Brand = mongoose.model('Brand', BrandSchema);
+
+export default Brand;
