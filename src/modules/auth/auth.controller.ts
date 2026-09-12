1be3ec908f3438fcfc69f1a3bcc16181bb0e086a
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-09-12 12:31:47 +0300

    feat: implement authentication controller with register, login, and logout functionalities

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,90 @@
+import { type Request, type Response } from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import User from './auth.model.js';
+export const register = async (req: Request, res: Response) => {
+    const { username, email, phoneNumber, password, role } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const newUser = new User({
+        username,
+        email,
+        phoneNumber,
+        password: hashedPassword,
+        role,
+    });
+    await newUser.save();
+    const token = jwt.sign(
+        {
+            userId: newUser._id,
+            role: newUser.role,
+        },
+        process.env.TOKEN_SECRET!,
+        {
+            algorithm: 'HS256',
+            expiresIn: '1m',
+        }
+    );
+    res.status(201).json({
+        status: "success",
+        data: {
+            newUser,
+            token,
+        }
+    });
+};
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(404).json({
+            status: "error",
+            message: "User not found",
+        });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+        return res.status(401).json({
+            status: "error",
+            message: "Invalid password",
+        });
+    }
+    const token = jwt.sign(
+        {
+            userId: user._id,
+            role: user.role,
+        },
+        process.env.TOKEN_SECRET!,
+        {
+            algorithm: 'HS256',
+            expiresIn: '1m',
+        }
+    );
+    res.status(200).json({
+        status: "success",
+        data: {
+            user,
+            token,
+        }
+    });
+};
+export const logout = async (req: Request, res: Response) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({
+            status: "error",
+            message: "Authorization header missing",
+        });
+    }
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({
+            status: "error",
+            message: "Token missing",
+        });
+    }
+    res.status(200).json({
+        status: "success",
+        message: "Logged out successfully",
+    });
+};
